d3a62f0b0d5b52693f6af60ec3ecb1bb04c57601
lakshmans97 <117919162+lakshmans97@users.noreply.github.com>
2022-11-16 12:54:07 +0530

    arrays

--- variable/sub.js
+++ variable/sub.js
@@ -1,7 +1,22 @@
 const a=20;
-const b=32;
-console.log(a-b);
+const b=6;
+const c=a/b;
+const d= 'lakshman' ;
+const ch=20;
+let ch1=266;
+ch1=20;
+console.log("sub of a&b",a-b);
 console.log(a+b);
 console.log(a*b);
-console.log(a/b);
+console.log(c);
 console.log(a^b);
+console.log(a%b);
+console.log(a==b);
+console.log(a!=b);
+console.log((a!=b) && (a==b));
+console.log((a!=b) || (a==b));
+console.log(d)
+console.log(ch);
+
+
+
